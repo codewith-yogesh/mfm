3c2bd1f0684c0f5ec49fe530091723fc3a9197ea
codewith-yogesh <yogeshwaran.manovate.co.in@gmail.com>
2025-11-04 22:37:29 +0530

    Removed old files

--- room-booking-backend/src/main/java/com/example/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
